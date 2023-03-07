6b55fad07cc98f3c7e441a86f4637a292023bce2
Adnan Khan <maaj20056@gmail.com>
2023-03-07 21:38:22 +0530

    Added login auth, authorisation and redirection

--- Gameio/Apps/urls.py
+++ Gameio/Apps/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('Login', views.Login, name='Login'),
     path('signup', views.signup, name='signup'),
     path('home', views.home, name='home'),
+    path('logout', views.Logout, name='logout')
 ]
