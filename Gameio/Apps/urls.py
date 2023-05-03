b44f2137bd457f1054dd0c5090ab18a6e679e7b1
Adnan Khan <maaj20056@gmail.com>
2023-05-03 17:28:52 +0530

    added home page

--- Gameio/Apps/urls.py
+++ Gameio/Apps/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('home', views.home, name='home'),
     path('logout', views.Logout, name='logout'),
-    path('Dashboard', views.Dashboard, name='Dashboard')
+    path('Dashboard', views.Dashboard, name='Dashboard'),
+    path('profile', views.profile, name='profile'),
+    path('Homepage', views.Homepage, name='Homepage'),
+    path('Snakerules', views.Snakerules, name='Snakerules'),
+    path('snake', views.snake, name='snake'),
+    path('gameover', views.gameover, name='gameover'),
 ]
--- Gameio/Apps/views.py
+++ Gameio/Apps/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
-
+from .models import Score
 # Create your views here.
 
 
@@ -10,6 +10,28 @@ from django.contrib.auth.decorators import login_required
 def Dashboard(request):
     return render(request, "Apps/Dashboard.html")
 
+
+@login_required(login_url='Login')
+def profile(request):
+    return render(request, "Apps/profile.html")
+
+@login_required(login_url='Login')
+def Homepage(request):
+    return render(request,"Apps/Homepage.html")
+
+@login_required(login_url='Login')
+def Snakerules(request):
+    return render(request, "Apps/Snakerules.html")
+
+
+@login_required(login_url='Login')
+def snake(request):    
+    return render(request, "Apps/snake.html")
+
+
+@login_required(login_url='Login')
+def gameover(request):
+    return render(request, "Apps/gameover.html")
 
 def home(request):
     return render(request, "Apps/home.html")
@@ -22,7 +44,7 @@ def Login(request):
         user = authenticate(request, username=username, password=pass1)
         if user is not None:
             login(request,user)
-            return redirect('Dashboard')
+            return redirect('Homepage')
         else:
             return render(request, 'login', {'error': ": Username or password is incorrect"})
     return render(request, "Apps/Login.html")
